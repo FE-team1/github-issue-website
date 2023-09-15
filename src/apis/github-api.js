18fdf0437ee98024b0cd7e67468ac90275449d0a
kiminn <808010x@gmail.com>
2023-09-16 02:56:51 +0900

    feat:페이지네이션 구현완

--- src/apis/github-api.js
+++ src/apis/github-api.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useIssueList = (pageNumber) => {
+// *** index.js에서 쓰였던 state page 
+// 이름바꾸기 우클릭 F2
+const useIssueList = (page) => {
   const [issue, setIssue] = useState([]); // 데이터 저장
+  // issue는 한 번만 불려와도됨
+  // subView는 전체 중에서 일부 (10개씩)
+  const [subView, setSubView] = useState([]);
+
+  const issueListApi = async () => {
+    const response = await axios.get(`https://api.github.com/repos/facebook/react/issues`, {
+      headers: { Authorization: "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" },
+    });
+    setIssue((prev) => {
+      return [...prev, ...response.data];
+    });
+  };
 
   useEffect(() => {
-    const issueListApi = async () => {
-      const response = await axios.get(`https://api.github.com/repos/facebook/react/issues`, {
-        headers: { Authorization: "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" },
-      });
-      setIssue((prev) => {
-        return [...prev, ...response.data];
-      });
-    };
-    issueListApi();
-  }, [pageNumber]);
-
-  return { issue };
+    // page가 바뀔때마다 이슈데이터 슬라이스 해서 세팅! 0*10 X
+    // subView가 바껴지면서 index.js에 바뀐형태로 나타나게됨
+    // 열개만 표시되게..ㅜ
+    setSubView(issue.slice(page, (page + 1) * 10));
+  }, [page]);
+  return [subView, issueListApi]
 };
 
 export default useIssueList;
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "../reducer";
-
-export const store = configureStore({
-    reducer: rootReducer,
-    devTools: process.env.NODE_ENV === 'development', // 사용자는 개발자도구를 볼 필요 없다  
-})
